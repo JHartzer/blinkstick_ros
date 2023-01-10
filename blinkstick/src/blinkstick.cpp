0f1f11c94dbe54746b0088919ee5d32feb421ad4
Jacob <JacobHartzer@gmail.com>
2023-01-09 21:37:34 -0600

    Implement node and tester

--- blinkstick/src/blinkstick.cpp
+++ blinkstick/src/blinkstick.cpp
@@ -8,24 +8,19 @@
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
 using std::placeholders::_1;
 
-class MinimalSubscriber : public rclcpp::Node
+class BlinkstickNode : public rclcpp::Node
 {
 public:
-  MinimalSubscriber()
-  : Node("minimal_subscriber")
+  BlinkstickNode()
+  : Node("blinkstick")
   {
     RCLCPP_INFO(this->get_logger(), "Initialize Node");
 
-    subscription_ = this->create_subscription<std_msgs::msg::String>(
-      "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+    m_subscriber = this->create_subscription<blinkstick_msgs::msg::Blink>(
+      "blinkstick", 10, std::bind(&BlinkstickNode::topic_callback, this, _1));
 
-    // int args_red = 255;
-    // int args_green = 255;
-    // int args_blue = 0;
-    // int args_index = 1;
     blinkstick::enable_logging();
     m_device = blinkstick::find();
     if (!m_device.is_valid()) {
@@ -34,49 +29,34 @@ public:
     }
 
     m_led_count = m_device.get_led_count();
-    std::cout << "There are " << m_led_count << " LEDs\n";
-
-    // set the color
-    bool red = true;
-    m_colours.resize(m_led_count);
-    for (int i = 0; i < m_led_count; ++i) {
-      auto & colour = m_colours[i];
-
-      if (red) {
-        //colour.red = 255;
-        colour.red = 255;
-        colour.green = 215;
-      } else {
-        //colour.green = 255;
-        colour.red = 64;
-        colour.green = 219;
-        colour.blue = 219;
-      }
-      red = !red;
-    }
   }
 
 private:
-  void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
+  void topic_callback(const blinkstick_msgs::msg::Blink::SharedPtr msg) const
   {
-    RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
-    m_device.set_colours(m_args_channel, m_colours);
-    // std::rotate(m_colours.begin(), m_colours.begin() + 1, m_colours.end());
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+
+    std::vector<blinkstick::colour> colors(m_led_count);
+    std::string log = "Subscriber callback. Size: " + std::to_string(colors.size());
+    RCLCPP_INFO(this->get_logger(), log.c_str());
+    for (unsigned int i = 0; i < colors.size(); ++i) {
+      colors[i].red = msg.get()->red;
+      colors[i].green = msg.get()->green;
+      colors[i].blue = msg.get()->blue;
+    }
+    m_device.set_colours(m_args_channel, colors);
   }
 
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+  rclcpp::Subscription<blinkstick_msgs::msg::Blink>::SharedPtr m_subscriber;
   int m_led_count {0};
-  std::vector<blinkstick::colour> m_colours;
   int m_args_channel {0};
   blinkstick::device m_device = blinkstick::device(nullptr, blinkstick::device_type::unknown);
-
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::spin(std::make_shared<BlinkstickNode>());
   rclcpp::shutdown();
   blinkstick::finalise();
   return 0;
---
+++ blinkstick/src/test_blinkstick.cpp
@@ -0,0 +1,50 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "blinkstick_msgs/msg/blink.hpp"
+#include <rclcpp/duration.hpp>
+
+using namespace std::chrono_literals;
+
+/* This example creates a subclass of Node and uses std::bind() to register a
+* member function as a callback from the timer. */
+
+class BlinkstickTester : public rclcpp::Node
+{
+public:
+  BlinkstickTester()
+  : Node("BlinkstickTester")
+  {
+    m_publisher = this->create_publisher<blinkstick_msgs::msg::Blink>("blinkstick", 10);
+    m_timer = this->create_wall_timer(
+      500ms, std::bind(&BlinkstickTester::m_timer_callback, this));
+  }
+
+private:
+  void m_timer_callback()
+  {
+    RCLCPP_INFO(this->get_logger(), "Timer callback: publishing");
+    auto message = blinkstick_msgs::msg::Blink();
+    message.red = m_colors[0];
+    message.green = m_colors[1];
+    message.blue = m_colors[2];
+    m_publisher->publish(message);
+    std::rotate(m_colors.begin(), m_colors.begin() + 1, m_colors.end());
+  }
+
+  rclcpp::TimerBase::SharedPtr m_timer;
+  rclcpp::Publisher<blinkstick_msgs::msg::Blink>::SharedPtr m_publisher;
+  std::vector<unsigned int> m_colors {255, 0, 0};
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<BlinkstickTester>());
+  rclcpp::shutdown();
+  return 0;
+}
