7235ce9629f31703a8ea7d664eeee13e9aeabc2e
Jacob <JacobHartzer@gmail.com>
2023-01-09 22:33:57 -0600

    Passing ament tests

--- blinkstick/src/blinkstick.cpp
+++ blinkstick/src/blinkstick.cpp
@@ -1,11 +1,24 @@
+// Copyright 2022 Jacob Hartzer
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #include <iostream>
 #include <memory>
 
 #include "blinkstick_msgs/msg/color.hpp"
 #include "blinkstick/device.hpp"
 #include "blinkstick/blinkstick.hpp"
-
-#include <memory>
 
 #include "rclcpp/rclcpp.hpp"
 using std::placeholders::_1;
@@ -34,8 +47,6 @@ public:
 private:
   void topic_callback(const blinkstick_msgs::msg::Color::SharedPtr msg) const
   {
-
-
     std::vector<blinkstick::colour> colors(m_led_count);
     std::string log = "Subscriber callback. Size: " + std::to_string(colors.size());
     RCLCPP_INFO(this->get_logger(), log.c_str());
